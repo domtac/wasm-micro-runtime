7643ce7ccbd97acb005b33c74a5e8ffc75205533
Dominik Tacke <dominik.tacke@siemens.com>
2025-11-14 12:39:47 +0100

    feat: adapt mini loader
    Signed-off-by: Dominik Tacke <dominik.tacke@siemens.com>

--- core/iwasm/interpreter/wasm_runtime.c
+++ core/iwasm/interpreter/wasm_runtime.c
@@ -314,6 +314,9 @@ memory_instantiate(WASMModuleInstance *module_inst, WASMModuleInstance *parent,
     default_max_page =
         memory->is_memory64 ? DEFAULT_MEM64_MAX_PAGES : DEFAULT_MAX_PAGES;
     default_max_page *= custom_pages_multiplier;
+    if (default_max_page == 0) {
+        default_max_page = UINT32_MAX;
+    }
 
     /* The app heap should be in the default memory */
     if (memory_idx == 0) {
