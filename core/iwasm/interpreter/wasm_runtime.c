efeebbbb17cf1fd0bd195b85448e96cb3ec95c1d
Dominik Tacke <dominik.tacke@siemens.com>
2026-02-12 17:42:14 +0000

    fix: uninitialized memory
    Signed-off-by: Dominik Tacke <dominik.tacke@siemens.com>

--- core/iwasm/interpreter/wasm_runtime.c
+++ core/iwasm/interpreter/wasm_runtime.c
@@ -310,7 +310,7 @@ memory_instantiate(WASMModuleInstance *module_inst, WASMModuleInstance *parent,
     }
 #endif
     uint32 custom_pages_multiplier =
-        DEFAULT_NUM_BYTES_PER_PAGE / memory->num_bytes_per_page;
+        DEFAULT_NUM_BYTES_PER_PAGE / num_bytes_per_page;
     default_max_page =
         memory->is_memory64 ? DEFAULT_MEM64_MAX_PAGES : DEFAULT_MAX_PAGES;
     default_max_page *= custom_pages_multiplier;
